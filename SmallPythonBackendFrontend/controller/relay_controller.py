93a0a27a3da2c394a77d7df5ae2bd9e92396a16b
StudentJavelLNU.se <dc222dz@student.lnu.se>
2020-02-20 12:21:49 +0100

    Will after lunch continue at "posting relay ONOROFF" And change values in file accordingly Then making arduino use this/turn on lights with 1 secund latency from webBrowser.

--- SmallPythonBackendFrontend/controller/relay_controller.py
+++ SmallPythonBackendFrontend/controller/relay_controller.py
@@ -22,3 +22,29 @@ class RelayController(Resource):
 
         #ELSE:
         return service.getRelay(apiKey)
+    def post(self):
+        if IS_DEBUG:
+            print("ARDUINO FUCK YES!")
+            print("FORMBAJS: ",request.headers)
+        
+        apiKey = request.headers["x-api-key"]
+        credentials = request.headers["Credentials"]
+        if IS_DEBUG:
+            print("Api key?: ",apiKey)
+            print("Credentials?: ",credentials)
+
+        if apiKey != "1337" or credentials != "BAJS":
+            eCode = 401
+            eMsg = "Not the correct credentials!"
+            return errorsToResponse.getResponse(eMsg,eCode)
+        data = request.json
+        if data == None:
+            eCode = 400
+            eMsg = "Incorrect formatt/Bad request!"
+            return errorsToResponse.getResponse(eMsg,eCode)
+        #ELSE:
+        if IS_DEBUG:
+            print("Api key?: ",apiKey)
+            print("Credentials?: ",credentials)
+            print("WONDERFULL DATA?:\n",data)
+        return "MAGNIFIC"
